82f7e73b1055214cb14b4674fdd024f42bcc23f0
NehuenYago <nehuenromero@protonmail.com>
2023-05-10 15:52:51 -0300

    ejercicio 2

--- HASKELL/guia5.hs
+++ HASKELL/guia5.hs
@@ -1,20 +1,88 @@
 -- Ejercicio 1
--- 1)
+-- 1.1)
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud (_:xs) = 1 + longitud xs
 
--- 2)
+-- 1.2)
 ultimo :: [t] -> t
 ultimo [x] = x
 ultimo (_:xs) = ultimo xs
 
--- 3)
+-- 1.3)
 principio :: [t] -> [t]
 principio [x] = []
 principio (x:xs) = x : principio xs
 
--- 4)
+-- 1.4)
 reverso :: [t] -> [t]
 reverso [] = []
 reverso (x:xs) = reverso xs ++ [x]
+
+-- Ejercicio 2
+-- 2.1)
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece e (x:xs)
+ | e == x = True
+ | otherwise = pertenece e xs
+
+--  2.2)
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales [_] = True
+todosIguales (x:xs) = x == head xs && todosIguales xs
+
+-- 2.3)
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos (x:xs) = (not (pertenece x xs)) && todosDistintos xs
+
+-- 2.4)
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) = pertenece x xs || hayRepetidos xs
+
+-- 2.4) otra forma
+-- hayRepetidos :: (Eq t) => [t] -> Bool
+-- hayRepetidos s = not (todosDistintos s)
+
+-- 2.5)
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar e (x:xs)
+ | e == x = xs
+ | otherwise = x : quitar e xs
+
+-- 2.6)
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos e (x:xs)
+ | e == x = quitarTodos e xs
+ | otherwise = x : quitarTodos e xs
+
+-- 2.7)
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) = x : (eliminarRepetidos (quitarTodos x xs))
+
+-- 2.8)
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos x y = verificaMismosElementos x y && verificaMismosElementos y x
+
+verificaMismosElementos :: (Eq t) => [t] -> [t] -> Bool
+verificaMismosElementos [x] y = pertenece x y
+verificaMismosElementos (x:xs) y = pertenece x y && verificaMismosElementos xs y
+
+-- 2.9)
+capicua :: (Eq t) => [t] -> Bool
+capicua s = s == reverso s
+
+-- 2.9) otra forma
+-- capicua [] = True
+-- capicua [_] = True
+-- capicua (x:xs) = (x == ultimo xs) && capicua (eliminaUltimo xs) 
+
+-- eliminaUltimo :: [t] -> [t]
+-- eliminaUltimo [_] = []
+-- eliminaUltimo (x:xs) = x : eliminaUltimo xs
