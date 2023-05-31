8d335599af08167d205361709a91e2983ec881b5
NehuenYago <nehuenromero@protonmail.com>
2023-05-30 22:51:00 -0300

    add 1.5)

--- PYTHON/guia7.py
+++ PYTHON/guia7.py
@@ -5,22 +5,19 @@ import math as math
 def raizDe2() -> float:
     return round(math.sqrt(2), 4)
 
-# print(raizDe2())
-
 # 1.2)
 def imprimirHola():
     print("hola")
-
-# imprimirHola()
 
 # 1.3)
 def imprimirUnVerso():
     print("Around the World, around the wooorld!\n")
 
-# imprimirUnVerso()
-
 # 1.4)
 def factorial2() -> int:
     return 2
 
-# print(factorial2())
+# 1.5)
+def factorial3() -> int:
+    return 3 * factorial2()
+
