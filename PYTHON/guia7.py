f172324410e78422cdb3e8206980a59e60bcfac1
NehuenYago <nehuenromero@protonmail.com>
2023-05-30 22:49:04 -0300

    add 1.3)

--- PYTHON/guia7.py
+++ PYTHON/guia7.py
@@ -7,7 +7,15 @@ def raizDe2() -> float:
 
 # print(raizDe2())
 
+# 1.2)
 def imprimirHola():
     print("hola")
 
 # imprimirHola()
+
+# 1.3)
+def imprimirUnVerso():
+    print("Around the World, around the wooorld!\n")
+
+# imprimirUnVerso()
+
