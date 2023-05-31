1e50b596d3ec3bd380debefedc3cfd3ff7094dfa
NehuenYago <nehuenromero@protonmail.com>
2023-05-31 14:46:18 -0300

    change 1.1) and add 1.3)

--- PYTHON/guia8.py
+++ PYTHON/guia8.py
@@ -1,8 +1,8 @@
 # Ejercicio 1
 # 1.1) Si se implementa con tipos genericos sirve para buscar una caracter en una string
 def pertenece (s: list, e: int) -> bool:
-    for i in range(len(s)):
-        if s[i] == e:
+    for i in s:
+        if i == e:
             return True
     return False
 
@@ -17,3 +17,8 @@ def divideATodos (s: list, e: int) -> bool:
     return True
 
 # 1.3)
+def sumaTotal (s:list) -> int:
+    total = 0
+    for i in s:
+        total += i
+    return total
