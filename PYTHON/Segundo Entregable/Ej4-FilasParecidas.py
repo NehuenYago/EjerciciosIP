55ef9fa86a70fb4d4a785fb7188bd6b4cc4a9a97
NehuenYago <nehuenromero@protonmail.com>
2023-06-02 12:39:45 -0300

    resolucion filasParecidas()

--- PYTHON/Segundo Entregable/Ej4-FilasParecidas.py
+++ PYTHON/Segundo Entregable/Ej4-FilasParecidas.py
@@ -1,23 +1,15 @@
 from typing import List
 
-# Aclaración: Debido a la versión de Python del CMS, para el tipo Lista, la sintaxis de la definición de tipos que deben usar es la siguiente:
-# l: List[int]  <--Este es un ejemplo para una lista de enteros.
-# Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
-def filasParecidas(matriz: List[List[int]]) -> bool :
-  # Implementar esta funcion
-  return True
+def filasParecidas(matriz: List[List[int]]) -> bool:
+    n:int = 0
 
-
-if __name__ == '__main__':
-  filas = int(input())
-  columnas = int(input())
- 
-  matriz = []
- 
-  for i in range(filas):         
-    fila = input()
-    if len(fila.split()) != columnas:
-      print("Fila " + str(i) + " no contiene la cantidad adecuada de columnas")
-    matriz.append([int(j) for j in fila.split()])
-  
-  print(filasParecidas(matriz))
+    if len(matriz) == 1:
+        return True
+    elif len(matriz) > 1:
+        n = matriz[1][0] - matriz[0][0]
+    
+    for i in range(1,len(matriz)):
+        for j in range(len(matriz[i])):
+            if not (matriz[i][j] == matriz[i-1][j] + n):
+                return False
+    return True
