58028ee4689e052d8ce39d1d25cf87dce961071b
NehuenYago <nehuenromero@protonmail.com>
2023-06-03 10:19:08 -0300

    generalizo el caso 'Empate'

--- PYTHON/Segundo Entregable/Ej1-quienGana.py
+++ PYTHON/Segundo Entregable/Ej1-quienGana.py
@@ -1,16 +1,14 @@
 def quienGana (j1: str, j2: str) -> str:
-    if j1 == "Piedra":
-        if j2 == "Piedra":
-            return "Empate"
-        elif j2 == "Papel":
+    if j1 == j2:
+        return "Empate"
+    elif j1 == "Piedra":
+        if j2 == "Papel":
             return "Jugador2"
         elif j2 == "Tijera":
             return "Jugador1"
     elif j1 == "Papel":
         if j2 == "Piedra":
             return "Jugador1"
-        elif j2 == "Papel":
-            return "Empate"
         elif j2 == "Tijera":
             return "Jugador2"
     elif j1 == "Tijera":
@@ -18,5 +16,3 @@ def quienGana (j1: str, j2: str) -> str:
             return "Jugador2"
         elif j2 == "Papel":
             return "Jugador1"
-        elif j2 == "Tijera":
-            return "Empate"
