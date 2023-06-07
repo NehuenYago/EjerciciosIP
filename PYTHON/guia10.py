8e304c3d073d65b92b9236b5017e5073d3be70dd
NehuenYago <nehuenromero@protonmail.com>
2023-06-07 20:00:31 -0300

    add 1.3)

--- PYTHON/guia10.py
+++ PYTHON/guia10.py
@@ -12,6 +12,16 @@ def contarLineas(archivo:str) -> int:
 # 1.2)
 def existePalabra(palabra:str, archivo:str) -> bool:
     texto = archivo.read()
-    if palabra in texto:
-        return True
+    for i in texto.split():
+        if i == palabra:
+            return True
     return False
+
+# 1.3)
+def cantidadApariciones(archivo:str, palabra:str) -> int:
+    texto = archivo.read()
+    apariciones: int = 0
+    for i in texto.split():
+        if i == palabra:
+            apariciones += 1
+    return apariciones
