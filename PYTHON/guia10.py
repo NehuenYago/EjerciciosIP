93dd735df466de5187ba4e23b66eda7e95fa41a2
NehuenYago <nehuenromero@protonmail.com>
2023-06-07 19:49:35 -0300

    add 1.2)

--- PYTHON/guia10.py
+++ PYTHON/guia10.py
@@ -2,9 +2,16 @@ f = open("/home/yago/Documents/repos/IP/PYTHON/input.txt","r")
 
 # Ejercicio 1
 # 1.1)
-def contarLineas(archivo: str) -> int:
+def contarLineas(archivo:str) -> int:
     lineas = archivo.readlines()
     l: int = 0
     for i in lineas:
         l += 1
     return l
+
+# 1.2)
+def existePalabra(palabra:str, archivo:str) -> bool:
+    texto = archivo.read()
+    if palabra in texto:
+        return True
+    return False
