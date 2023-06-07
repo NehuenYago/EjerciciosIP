85d67a969384581d99d8f5eabfe15e43e3a0615b
NehuenYago <nehuenromero@protonmail.com>
2023-06-07 20:10:56 -0300

    change 1.1) and 1.2)

--- PYTHON/guia10.py
+++ PYTHON/guia10.py
@@ -3,25 +3,25 @@ f = open("/home/yago/Documents/repos/IP/PYTHON/input.txt","r")
 # Ejercicio 1
 # 1.1)
 def contarLineas(archivo:str) -> int:
-    lineas = archivo.readlines()
     l: int = 0
-    for i in lineas:
+    for i in archivo.readlines():
         l += 1
     return l
 
 # 1.2)
 def existePalabra(palabra:str, archivo:str) -> bool:
-    texto = archivo.read()
-    for i in texto.split():
-        if i == palabra:
-            return True
-    return False
+    return palabra in archivo.read().split()
 
 # 1.3)
-def cantidadApariciones(archivo:str, palabra:str) -> int:
+def cantidadApariciones(palabra:str, archivo:str) -> int:
     texto = archivo.read()
     apariciones: int = 0
+
     for i in texto.split():
         if i == palabra:
             apariciones += 1
     return apariciones
+
+# Ejercicio 2
+# def clonarSinComentarios(archivo:str):
+    
