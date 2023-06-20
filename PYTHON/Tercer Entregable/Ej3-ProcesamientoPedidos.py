4ca56a37a11b533ba30bd7324397efca7d7b4209
NehuenYago <nehuenromero@protonmail.com>
2023-06-20 18:59:50 -0300

    corrijo para que acepte queue

--- PYTHON/Tercer Entregable/Ej3-ProcesamientoPedidos.py
+++ PYTHON/Tercer Entregable/Ej3-ProcesamientoPedidos.py
@@ -9,17 +9,18 @@ def procesamiento_pedidos(pedidos: Queue,
                           stock_productos: Dict[str, int],
                           precios_productos: Dict[str, float]) -> List[Dict[str, Union[int, str, float, Dict[str, int]]]]:
     
-    lista_pedidos_procesados:List[Dict[str, Union[int, str, float, Dict[str, int]]]] = []
+    lista_pedidos_procesados = []
 
-    for i in range(len(pedidos)):
-        pedido_procesado: Dict[str, Union[int, str, float, Dict[str, int]]] = {}
-        productos: Dict[str,int] = pedidos[i].get('productos')
-        productos_recibidos: Dict[str,int] = {}
-        precio_total:int = 0
-        estado:str = 'completo'
+    while not pedidos.empty():
+        pedido = pedidos.get()
+        pedido_procesado = {}
+        productos = pedido.get("productos")
+        productos_recibidos = {}
+        precio_total = 0
+        estado = "completo"
 
         for producto in productos:
-            cantidad_recibida: int = 0
+            cantidad_recibida = 0
             while stock_productos.get(producto) > 0 and productos.get(producto) > 0:
                 precio_total += precios_productos.get(producto)
                 stock_productos[producto] -= 1
@@ -29,20 +30,26 @@ def procesamiento_pedidos(pedidos: Queue,
             productos_recibidos[producto] = cantidad_recibida
 
             if productos.get(producto) > 0:
-                estado = 'incompleto'
+                estado = "incompleto"
         
-        pedido_procesado['id'] = pedidos[i].get("id")
-        pedido_procesado['cliente'] = pedidos[i].get("cliente")
-        pedido_procesado['productos'] = productos_recibidos
-        pedido_procesado['precio_total'] = precio_total
-        pedido_procesado['estado'] = estado
+        pedido_procesado["id"] = pedido.get("id")
+        pedido_procesado["cliente"] = pedido.get("cliente")
+        pedido_procesado["productos"] = productos_recibidos
+        pedido_procesado["precio_total"] = precio_total
+        pedido_procesado["estado"] = estado
 
-        lista_pedidos_procesados += [pedido_procesado]
+        lista_pedidos_procesados.append(pedido_procesado)
 
     return lista_pedidos_procesados
 
 
-pedidos = [{"id":21,"cliente":"Gabriela", "productos":{"Manzana":2}}, {"id":1,"cliente":"Juan","productos":{"Manzana":2,"Pan":4,"Factura":6}}]
-stock_productos = {"Manzana":10, "Leche":5, "Pan":3, "Factura":0}
+def cola(p):
+    cola: Queue = Queue()
+    for i in p:
+        cola.put(i)
+    return cola
+
+pedidos = [{"id":21,"cliente":"Gabriela", "productos":{"Manzana":2}}, {"id":1,"cliente":"Juan","productos":{"Manzana":2, "Pan":1,"Manzana":10}}]
+stock_productos = {"Manzana":9, "Leche":5, "Pan":3, "Factura":0}
 precios_productos = {"Manzana":3.5, "Leche":5.5, "Pan":3.5, "Factura":5}
-print(procesamiento_pedidos(pedidos, stock_productos, precios_productos))
+print(procesamiento_pedidos(cola(pedidos), stock_productos, precios_productos))
