a74a9903fadab585c6e6f4ed433e38a1a4e9fdc0
NehuenYago <nehuenromero@protonmail.com>
2023-06-23 19:49:36 -0300

    borro print y comentarios y agrego algunos tipados

--- PYTHON/Tercer Entregable/Ej4-FilaDelBanco.py
+++ PYTHON/Tercer Entregable/Ej4-FilaDelBanco.py
@@ -1,12 +1,11 @@
 from queue import Queue
 
-# El tipo de fila debería ser Queue[int], pero la versión de python del CMS no lo soporta. Usaremos en su lugar simplemente "Queue"
-def avanzarFila(fila: Queue, min: int):
+def avanzarFila(fila: Queue[int], min: int):
     cantidadInicial = fila.qsize()
     minutos: int = 0
 
     minutoVuelveFilaCliente:int = -1
-    clienteDeCaja3 = 0
+    clienteDeCaja3:int = 0
 
     while minutos <= min:
             
@@ -30,17 +29,3 @@ def avanzarFila(fila: Queue, min: int):
         minutos += 1
 
     return fila
-
-
-def cola(p):
-    cola: Queue = Queue()
-    for i in p:
-        cola.put(i)
-    return cola
-
-print(list(avanzarFila(cola([1,2,3]), 100).queue))
-
-# Caja1: Empieza a atender 10:01, y atiende a una persona cada 10 minutos
-# Caja2: Empieza a atender 10:03, atiende a una persona cada 4 minutos
-# Caja3: Empieza a atender 10:02, y atiende una persona cada 4 minutos, pero no le resuelve el problema y la persona debe volver a la fila (se va al final y tarda 3 min en llegar. Es decir, la persona que fue atendida 10:02 vuelve a entrar a la fila a las 10:05)
-# La fila empieza con las n personas que llegaron antes de que abra el banco. Cuando abre (a las 10), cada 4 minutos llega una nueva persona a la fila (la primera entra a las 10:00)
