947e96d961cc8948e51fd0d931d83e0525d32e6d
NehuenYago <nehuenromero@protonmail.com>
2023-06-27 11:43:49 -0300

    add types, change variable names, delete input tests

--- PYTHON/Tercer Entregable/Ej3-ProcesamientoPedidos.py
+++ PYTHON/Tercer Entregable/Ej3-ProcesamientoPedidos.py
@@ -1,55 +1,34 @@
 from queue import Queue
-from typing import List
-from typing import Dict
-from typing import Union
 
-# ACLARACIÓN: El tipo de "pedidos" debería ser: pedidos: Queue[Dict[str, Union[int, str, Dict[str, int]]]]
-# Por no ser soportado por la versión de CMS, usamos simplemente "pedidos: Queue"
-def procesamiento_pedidos(pedidos: Queue,
-                          stock_productos: Dict[str, int],
-                          precios_productos: Dict[str, float]) -> List[Dict[str, Union[int, str, float, Dict[str, int]]]]:
+def procesamiento_pedidos(pedidos: Queue[dict[str, [int, str, dict[str, int]]]],
+                          stockProductos: dict[str, int],
+                          preciosProductos: dict[str, float]) -> list[dict[str, [int, str, float, dict[str, int]]]]:
     
-    lista_pedidos_procesados = []
+    listaPedidosProcesados: list[dict[str, [int, str, float, dict[str, int]]]] = []
 
     while not pedidos.empty():
-        pedido = pedidos.get()
-        pedido_procesado = {}
-        productos = pedido.get("productos")
-        productos_recibidos = {}
-        precio_total = 0
-        estado = "completo"
+        pedido: dict[str, [int, str, dict[str, int]]] = pedidos.get()
+        diccionarioProductos: dict[str, int] = pedido["productos"]
+        precioTotal: int = 0
+        estado: str = "completo"
 
-        for producto in productos:
-            cantidad_recibida = 0
-            while stock_productos.get(producto) > 0 and productos.get(producto) > 0:
-                precio_total += precios_productos.get(producto)
-                stock_productos[producto] -= 1
-                productos[producto] -= 1
-                cantidad_recibida += 1
+        for producto in diccionarioProductos:
+            cantidadRecibida: int = 0
+            while stockProductos[producto] > 0 and diccionarioProductos[producto] > 0:
+                precioTotal += preciosProductos[producto]
+                stockProductos[producto] -= 1
+                diccionarioProductos[producto] -= 1
+                cantidadRecibida += 1
                                 
-            productos_recibidos[producto] = cantidad_recibida
-
-            if productos.get(producto) > 0:
+            if diccionarioProductos[producto] > 0:
                 estado = "incompleto"
+            
+            diccionarioProductos[producto] = cantidadRecibida
         
-        pedido_procesado["id"] = pedido.get("id")
-        pedido_procesado["cliente"] = pedido.get("cliente")
-        pedido_procesado["productos"] = productos_recibidos
-        pedido_procesado["precio_total"] = precio_total
-        pedido_procesado["estado"] = estado
+        pedido["productos"] = diccionarioProductos
+        pedido["precio_total"] = precioTotal
+        pedido["estado"] = estado
 
-        lista_pedidos_procesados.append(pedido_procesado)
+        listaPedidosProcesados.append(pedido)
 
-    return lista_pedidos_procesados
-
-
-def cola(p):
-    cola: Queue = Queue()
-    for i in p:
-        cola.put(i)
-    return cola
-
-pedidos = [{"id":21,"cliente":"Gabriela", "productos":{"Manzana":2}}, {"id":1,"cliente":"Juan","productos":{"Manzana":2, "Pan":1,"Manzana":10}}]
-stock_productos = {"Manzana":9, "Leche":5, "Pan":3, "Factura":0}
-precios_productos = {"Manzana":3.5, "Leche":5.5, "Pan":3.5, "Factura":5}
-print(procesamiento_pedidos(cola(pedidos), stock_productos, precios_productos))
+    return listaPedidosProcesados
